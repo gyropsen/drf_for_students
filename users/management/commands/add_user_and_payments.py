ac51d5b1a46f5c4605301d4e536515a6c9961802
egor <sharapov.egor00@gmail.com>
2024-05-30 14:57:18 +0300

    Fix add_user_and_payments.py

--- users/management/commands/add_user_and_payments.py
+++ users/management/commands/add_user_and_payments.py
@@ -8,7 +8,18 @@ class Command(BaseCommand):
     help = "Runs APScheduler."
 
     def handle(self, *args, **options):
-        user = User.objects.get(pk=1)
+        user = User.objects.create(
+            email="user2@gmail.com",
+            first_name="user2",
+            last_name="user2",
+            is_superuser=False,
+            is_staff=False,
+            is_active=True,
+        )
+
+        user.set_password("12345")
+        user.save()
+
         payment1 = Payment.objects.create(user=user, paid_course=Course.objects.get(pk=2), amount=1000, method="CASH")
         payment2 = Payment.objects.create(user=user, paid_lesson=Lesson.objects.get(pk=6), amount=1000)
         payment1.save()
