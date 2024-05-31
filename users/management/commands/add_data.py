18120c58e8eb69524c0e5604cdf1d98ee0b0529a
egor <sharapov.egor00@gmail.com>
2024-05-31 13:55:12 +0300

    Fix add_user_and_payments.py

--- users/management/commands/add_data.py
+++ users/management/commands/add_data.py
@@ -9,9 +9,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email="user2@gmail.com",
-            first_name="user2",
-            last_name="user2",
+            email="user3@gmail.com",
+            first_name="user3",
+            last_name="user3",
             is_superuser=False,
             is_staff=False,
             is_active=True,
@@ -20,7 +20,13 @@ class Command(BaseCommand):
         user.set_password("12345")
         user.save()
 
-        payment1 = Payment.objects.create(user=user, paid_course=Course.objects.get(pk=2), amount=1000, method="CASH")
-        payment2 = Payment.objects.create(user=user, paid_lesson=Lesson.objects.get(pk=6), amount=1000)
+        course = Course.objects.create(name="DRF")
+        lesson = Lesson.objects.create(name="serializers", course=course)
+
+        course.save()
+        lesson.save()
+
+        payment1 = Payment.objects.create(user=user, paid_course=course, amount=1000, method="CASH")
+        payment2 = Payment.objects.create(user=user, paid_lesson=lesson, amount=1000)
         payment1.save()
         payment2.save()
