451e287637a5801df065f779e415916e5a4b932c
egor <sharapov.egor00@gmail.com>
2024-06-05 18:42:58 +0300

    add perms for users

--- users/views.py
+++ users/views.py
@@ -2,10 +2,12 @@ from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
-
+from django.shortcuts import get_object_or_404
 from users.models import User, Payment
-from users.serializers import UserSerializer, PaymentSerializer, UserDetailSerializer
+from users.permissions import IsSelf
+from users.serializers import UserSerializer, PaymentSerializer, UserDetailSerializer, UserShortSerializer
 
 
 # User
@@ -18,7 +20,9 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     def get_serializer_class(self):
-        if self.action == "retrieve":
+        if self.action == 'list':
+            return UserShortSerializer
+        elif self.action == 'retrieve':
             return UserDetailSerializer
         return self.serializer_class
 
@@ -27,15 +31,24 @@ class UserViewSet(ModelViewSet):
         Instantiates and returns the create of permissions that this view requires.
         """
         if self.action == 'create':
-            permission_classes = (AllowAny, )
+            permission_classes = (AllowAny,)
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = (IsAuthenticated, IsSelf)
         else:
-            permission_classes = (IsAuthenticated, )
+            permission_classes = (IsAuthenticated,)
         return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         new_user = serializer.save(is_active=True)
         new_user.set_password(new_user.password)
         super().perform_create(new_user)
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset(), pk=self.kwargs["pk"])
+        if obj == self.request.user:
+            self.check_object_permissions(self.request, obj)
+            return obj
+        raise PermissionDenied
 
 
 # # # generics
